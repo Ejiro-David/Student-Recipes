04f0ae484f2b8b4523ed6a3670836e871589f231
Ejiro David <daviderhabor.de@gmail.com>
2022-02-03 22:48:20 +0100

    lorem ipsum

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body : ['Yanone Kaffeesatz']
+      }
+    },
+  },
+  plugins: [],
+}
